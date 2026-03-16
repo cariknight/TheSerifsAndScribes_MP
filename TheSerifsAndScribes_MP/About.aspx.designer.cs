45fae091ced0231612d921f0bb49f475773990f5
Rica <rica.caballero2006@gmail.com>
2026-03-16 09:11:12 +0800

    About Page Simple Layout

--- TheSerifsAndScribes_MP/About.aspx.designer.cs
+++ TheSerifsAndScribes_MP/About.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace TheSerifsAndScribes_MP
     {
 
         /// <summary>
+        /// Navbar1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UserControl Navbar1;
+
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
