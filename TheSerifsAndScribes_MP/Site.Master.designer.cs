ca11d4c08ecca9123a212be4eb5c5ea47c968adc
Rica <rica.caballero2006@gmail.com>
2026-03-18 22:37:12 +0800

    -updated tho same navbar problem

--- TheSerifsAndScribes_MP/Site.Master.designer.cs
+++ TheSerifsAndScribes_MP/Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace TheSerifsAndScribes_MP
     {
 
         /// <summary>
+        /// HeadContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -22,5 +31,14 @@ namespace TheSerifsAndScribes_MP
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
